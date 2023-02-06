55f1405143901cc5385fca90a4c0b7cb0cb035ba
Andrii Artemenko <artemenko.andrey22@gmail.com>
2023-02-06 21:30:01 +0100

    fix: task 1

--- index.js
+++ index.js
@@ -1,83 +1,75 @@
-function  makeDeepCopy (obj) {
-  if (typeof obj !== 'object' || obj === null) {
-    throw new Error()
+function makeDeepCopy(obj) {
+  if (obj === null || typeof obj !== 'object') {
+    throw new Error();
   }
 
-  let resultObj = {}
-
-  if (obj instanceof Array) {
-    resultObj = []
-    for (let i = 0, length = obj.length; i < length; i++) {
-      resultObj[i] = makeDeepCopy(obj[i])
+  let copyObj;
+  if (obj instanceof Date) {
+    copyObj = new Date(obj.getTime());
+  } else if (Array.isArray(obj)) {
+    copyObj = [];
+    for (let value of obj) {
+      copyObj.push(makeDeepCopy(value));
     }
-    return resultObj
-  }
-  else if (obj instanceof Date) {
-    resultObj = new Date()
-    resultObj.setTime(obj.getTime())
-    return resultObj
-  }
-  else if (obj instanceof Map) {
-    resultObj = new Map()
-    obj.forEach((val, key) => {resultObj.set(makeDeepCopy(key), makeDeepCopy(val))})
-    return resultObj
-  }
-  else if (obj instanceof Set) {
-    resultObj = new Set()
-    obj.forEach(val => {resultObj.add(makeDeepCopy(val))})
-    return resultObj
-  }
-  else if (obj instanceof Object) {
-    resultObj = {}
-    for (const key in obj) {
-      if (Object.prototype.hasOwnProperty.call(obj, key)) {
-        resultObj[key] = makeDeepCopy(obj[key])
-      }
+  } else if (obj instanceof Map) {
+    copyObj = new Map();
+    for (let [key, value] of obj) {
+      copyObj.set(key, makeDeepCopy(value));
     }
-    return resultObj
-  }
-}
-
-function selectFromInterval(arr, firstRange, secondRange) {
-  if ((!Array.isArray(arr)) ||
-    (!arr.every(Number)) ||
-    arr.length === 0 ||
-    typeof firstRange !== 'number' ||
-    typeof secondRange !== 'number') {
-      throw new Error()
-  }
-
-  let outerArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (firstRange > secondRange) {
-      if (arr[i] <= firstRange && arr[i] >= secondRange) {
-        outerArr.push(arr[i]);
-      }
-    } else if (arr[i] <= secondRange && arr[i] >= firstRange) {
-      outerArr.push(arr[i]);
+  } else if (obj instanceof Set) {
+    copyObj = new Set();
+    for (let value of obj) {
+      copyObj.add(makeDeepCopy(value));
+    }
+  }  else {
+    copyObj = {};
+    for (let key in obj) {
+      copyObj[key] = makeDeepCopy(obj[key]);
     }
   }
-  return outerArr
+  return copyObj;
 }
 
-function createIterable(from, to) {
-  if (isNaN(from) || isNaN(to) || from >= to) {
-    throw new Error()
-  }
-  else {
-    const obj = {
-      from: from,
-      to: to,
-    };
-    obj[Symbol.iterator] = function () {
-      return {
-        current: this.from,
-        last: this.to,
-        next() {
-          return (this.current <= this.last) ? {done: false, value: this.current++} : {done: true};
-        }
-      }
-    };
-    return obj;
-  }
-}
+// function selectFromInterval(arr, firstRange, secondRange) {
+//   if ((!Array.isArray(arr)) ||
+//     (!arr.every(Number)) ||
+//     arr.length === 0 ||
+//     typeof firstRange !== 'number' ||
+//     typeof secondRange !== 'number') {
+//       throw new Error();
+//   }
+//
+//   let outerArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (firstRange > secondRange) {
+//       if (arr[i] <= firstRange && arr[i] >= secondRange) {
+//         outerArr.push(arr[i]);
+//       }
+//     } else if (arr[i] <= secondRange && arr[i] >= firstRange) {
+//       outerArr.push(arr[i]);
+//     }
+//   }
+//   return outerArr;
+// }
+//
+// function createIterable(from, to) {
+//   if (isNaN(from) || isNaN(to) || from >= to) {
+//     throw new Error();
+//   }
+//   else {
+//     const obj = {
+//       from: from,
+//       to: to,
+//     }
+//     obj[Symbol.iterator] = function () {
+//       return {
+//         current: this.from,
+//         last: this.to,
+//         next() {
+//           return (this.current <= this.last) ? {done: false, value: this.current++} : {done: true};
+//         }
+//       }
+//     }
+//     return obj;
+//   }
+// }
