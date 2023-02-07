f20c28f2204e6c512fb90851eecfff7562b66638
Andrii Artemenko <artemenko.andrey22@gmail.com>
2023-02-07 13:53:25 +0100

    fix:uncomment second and third tasks

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function makeDeepCopy(obj) {
     for (let value of obj) {
       copyObj.add(makeDeepCopy(value));
     }
-  }  else {
+  } else {
     copyObj = {};
     for (let key in obj) {
       copyObj[key] = makeDeepCopy(obj[key]);
@@ -30,46 +30,45 @@ function makeDeepCopy(obj) {
   return copyObj;
 }
 
-// function selectFromInterval(arr, firstRange, secondRange) {
-//   if ((!Array.isArray(arr)) ||
-//     (!arr.every(Number)) ||
-//     arr.length === 0 ||
-//     typeof firstRange !== 'number' ||
-//     typeof secondRange !== 'number') {
-//       throw new Error();
-//   }
-//
-//   let outerArr = [];
-//   for (let i = 0; i < arr.length; i++) {
-//     if (firstRange > secondRange) {
-//       if (arr[i] <= firstRange && arr[i] >= secondRange) {
-//         outerArr.push(arr[i]);
-//       }
-//     } else if (arr[i] <= secondRange && arr[i] >= firstRange) {
-//       outerArr.push(arr[i]);
-//     }
-//   }
-//   return outerArr;
-// }
-//
-// function createIterable(from, to) {
-//   if (isNaN(from) || isNaN(to) || from >= to) {
-//     throw new Error();
-//   }
-//   else {
-//     const obj = {
-//       from: from,
-//       to: to,
-//     }
-//     obj[Symbol.iterator] = function () {
-//       return {
-//         current: this.from,
-//         last: this.to,
-//         next() {
-//           return (this.current <= this.last) ? {done: false, value: this.current++} : {done: true};
-//         }
-//       }
-//     }
-//     return obj;
-//   }
-// }
+function selectFromInterval(arr, firstRange, secondRange) {
+  if ((!Array.isArray(arr)) ||
+    (!arr.every(Number)) ||
+    arr.length === 0 ||
+    typeof firstRange !== 'number' ||
+    typeof secondRange !== 'number') {
+    throw new Error();
+  }
+
+  let outerArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (firstRange > secondRange) {
+      if (arr[i] <= firstRange && arr[i] >= secondRange) {
+        outerArr.push(arr[i]);
+      }
+    } else if (arr[i] <= secondRange && arr[i] >= firstRange) {
+      outerArr.push(arr[i]);
+    }
+  }
+  return outerArr;
+}
+
+function createIterable(from, to) {
+  if (isNaN(from) || isNaN(to) || from >= to) {
+    throw new Error();
+  } else {
+    const obj = {
+      from: from,
+      to: to,
+    };
+    obj[Symbol.iterator] = function () {
+      return {
+        current: this.from,
+        last: this.to,
+        next() {
+          return (this.current <= this.last) ? {done: false, value: this.current++} : {done: true};
+        }
+      };
+    };
+    return obj;
+  }
+}
